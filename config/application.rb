ecafcc804148f7ca2e606f4874d810afe81a70fb
vagrant <vagrant@localhost.localdomain>
2020-05-19 16:58:17 +0000

    [add]rails-i18nを適用するための記述を追加

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module SKILLBarter
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # デフォルトのlocaleを日本語(:ja)にする。
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{ur,yml}'.to_s)]
   end
 end
