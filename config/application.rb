a7b797b6168a943d1e8087835380ecd82e7473cb
vagrant <vagrant@localhost.localdomain>
2020-05-19 17:01:51 +0000

    [fix]i18nのpathの記述を修正

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,6 @@ module SKILLBarter
     # the framework and any gems in your application.
     # デフォルトのlocaleを日本語(:ja)にする。
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{ur,yml}'.to_s)]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml'.to_s)]
   end
 end
