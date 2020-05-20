438510b3b0129062a88d3187387d952e3a54bedd
utkazu <hiroshimazeal@gmail.com>
2020-05-20 07:38:33 +0000

    [add]deviseのuserモデルをmigrate。deviseのcontrollerとviewファイルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_20_073118) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name", null: false
+    t.string "kana_name", null: false
+    t.date "birthday"
+    t.string "phone_number", null: false
+    t.text "introduction"
+    t.string "profile_image_id"
+    t.integer "user_status", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
