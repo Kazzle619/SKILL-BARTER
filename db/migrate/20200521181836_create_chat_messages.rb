069dcf7427af445ccfe358a6ef27267dcff3e819
utkazu <hiroshimazeal@gmail.com>
2020-05-21 18:29:54 +0000

    [add]ChatMessageモデル完成。テスト記述完了。

---
+++ db/migrate/20200521181836_create_chat_messages.rb
@@ -0,0 +1,12 @@
+class CreateChatMessages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :chat_messages do |t|
+      t.references :proposition, null: false, foreign_key: true
+      t.references :room, null: false, foreign_key: true
+      t.text :content
+      t.string :image_id
+
+      t.timestamps
+    end
+  end
+end
