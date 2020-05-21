cd2fe60046c09461aac76211ae2a2933a51a2c00
utkazu <hiroshimazeal@gmail.com>
2020-05-21 12:52:31 +0000

    [update]enumにコメントを追加

--- app/models/background_school.rb
+++ app/models/background_school.rb
@@ -2,17 +2,26 @@ class BackgroundSchool < ApplicationRecord
   belongs_to :user
 
   enum school_type: {
+    # 高校
     highschool: 1,
+    # 専門学校
     vocational_school: 2,
+    # 短大
     junior_college: 3,
+    # 大学
     university: 4,
+    # 大学校
     academy: 5,
+    # 大学院
     graduate_school: 6,
   }
 
   enum enrollment_status: {
+    # 在籍中
     currently: 1,
+    # 卒業済
     graduated: 2,
+    # 中退
     dropout: 3,
   }
 
