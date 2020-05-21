ae5650c979c6f3b9068dc307348f5bb16c8afcc8
utkazu <hiroshimazeal@gmail.com>
2020-05-21 18:14:44 +0000

    [fix]アソシエーションテストのitがN:1になっていたので修正

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe Room, type: :model do
   describe 'アソシエーションのテスト' do
     context 'ChatMessageモデルとの関係' do
-      it 'N:1となっている' do
+      it '1:Nとなっている' do
         expect(Room.reflect_on_association(:chat_messages).macro).to eq :has_many
       end
     end
 
     context 'PropositionRoomモデルとの関係' do
-      it 'N:1となっている' do
+      it '1:Nとなっている' do
         expect(Room.reflect_on_association(:proposition_rooms).macro).to eq :has_many
       end
     end
