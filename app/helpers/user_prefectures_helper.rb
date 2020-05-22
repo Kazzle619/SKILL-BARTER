9f6447a4eac32190c379b8f23ae335baaad83a34
utkazu <hiroshimazeal@gmail.com>
2020-05-22 03:25:09 +0000

    [add]user_prefecturesコントローラ完成。必要なアクションを記載。â

---
+++ app/helpers/user_prefectures_helper.rb
@@ -0,0 +1,2 @@
+module UserPrefecturesHelper
+end
---
+++ test/controllers/user_prefectures_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserPrefecturesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
