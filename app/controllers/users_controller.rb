239edbc9f01c56dc65be4ad42578e1e659e15d9b
utkazu <hiroshimazeal@gmail.com>
2020-05-22 03:23:04 +0000

    [add]usersコントローラ完成。必要なアクションを記載。â

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  def top
+  end
+
+  def index
+  end
+
+  def mypage
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def blocking
+  end
+
+  def followers
+  end
+
+  def following
+  end
+
+  def notice
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/top'
+  get 'users/index'
+  get 'users/mypage'
+  get 'users/edit'
+  get 'users/show'
+  get 'users/blocking'
+  get 'users/followers'
+  get 'users/following'
+  get 'users/notice'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
