06aab576865a116a089b878bf3365ef789ffa4cb
utkazu <hiroshimazeal@gmail.com>
2020-05-24 06:51:41 +0000

    [add]モデル内でcurrent_userを使用するために関数を定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :kana_name, :phone_number])
+  end
+
+  # User.currentにcurrent_userを割り当てる。
+  def set_current_user
+    User.current = current_user
   end
 end
