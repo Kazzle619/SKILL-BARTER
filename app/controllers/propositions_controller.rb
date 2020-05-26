782577243792e28f53adfea115399261d839abf3
utkazu <hiroshimazeal@gmail.com>
2020-05-26 06:40:10 +0000

    [update]マッチング中の案件のみ表示されるよう条件を修正

--- app/controllers/propositions_controller.rb
+++ app/controllers/propositions_controller.rb
@@ -1,6 +1,11 @@
 class PropositionsController < ApplicationController
   def index
-    @propositions = Proposition.page(params[:page]).per(8).reverse_order
+    # 行が長すぎてrubocopで弾かれるので、2行で記述。必要なのは@propositions
+
+    # まだマッチしていない案件(交換ステータスが「マッチング中」か「交換申請中」)のみ表示。
+    # ORを使う場合はenumの文字列では検索できないようなので1(matching)と2(offering)で検索。
+    unmatched_propositions = Proposition.where("(barter_status = ?) OR (barter_status = ?)", 1, 2)
+    @propositions = unmatched_propositions.page(params[:page]).per(8).reverse_order
   end
 
   def create
