f2c2bdfc0597c43db19ef22155782db847286722
utkazu <hiroshimazeal@gmail.com>
2020-06-08 03:10:52 +0000

    [update]本番環境でも画像を適切に保存・表示できるよう参照元をpublibに変更。

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,7 +18,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
-    "skill_barter_logo.jpg"
+    "/images/skill_barter_logo.jpg"
   end
 
   # Process files as they are uploaded:
