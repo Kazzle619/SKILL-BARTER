3f1e0364f9b8860f07af4c30edd3fa091b7d1f3b
utkazu <hiroshimazeal@gmail.com>
2020-06-10 12:37:10 +0000

    [fix]name, kana_nameカラムのバリデーションの変更に合わせて修正。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name { Faker::Lorem.characters(number: 10) }
-    kana_name { Faker::Lorem.characters(number: 20) }
+    name { "#{Faker::Lorem.characters(number: 5)} #{Faker::Lorem.characters(number: 5)}" }
+    kana_name { "#{Faker::Lorem.characters(number: 5)} #{Faker::Lorem.characters(number: 5)}" }
     birthday { Faker::Date.birthday(min_age: 18, max_age: 40) }
     phone_number { Faker::PhoneNumber.cell_phone.delete("-") }
     introduction { Faker::Lorem.characters(number: 30) }
