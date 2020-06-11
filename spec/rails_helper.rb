bb7fbb4e7b263f1b12fc8a3fcca3f14978bbf0bf
utkazu <hiroshimazeal@gmail.com>
2020-06-11 03:37:39 +0000

    [add]deviseのメソッドを使用できるように記述を追加。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,4 +62,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+
+  # Deviseのメソッドをrequest spec内で使用できるように
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
