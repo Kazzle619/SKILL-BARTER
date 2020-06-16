cae2b7343b743e2549b05f1e338367dae44882c5
utkazu <hiroshimazeal@gmail.com>
2020-06-16 04:37:53 +0000

    [finish]follows_controllerの各メソッドへのアクセステストを記述。

---
+++ spec/requests/follows_request_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe "Follows", type: :request do
+  let(:follower) { create(:user) }
+  let(:followed) { create(:user) }
+
+  describe "POST #create" do
+    context "未ログインの場合" do
+      it "ログインページへリダイレクトすること" do
+        post user_follows_path(followed.id), xhr: true
+        # 非同期通信の場合、リダイレクトはされずエラーが出る。
+        expect(response.status).to eq 401
+      end
+    end
+
+    context "ログインしている場合" do
+      before do
+        sign_in follower
+      end
+
+      it "リクエストが成功すること" do
+        post user_follows_path(followed.id), xhr: true
+        expect(response).to have_http_status "200"
+      end
+      it "フォロー関係の作成に成功すること" do
+        expect do
+          post user_follows_path(followed.id), xhr: true
+        end.to change(Follow, :count).by(1)
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let!(:follow) { Follow.create(follower_id: follower.id, followed_id: followed.id) }
+
+    context "未ログインの場合" do
+      it "ログインページへリダイレクトすること" do
+        delete user_follow_path(followed.id, follow.id), xhr: true
+        # 非同期通信の場合、リダイレクトはされずエラーが出る。
+        expect(response.status).to eq 401
+      end
+    end
+
+    context "ログインしている場合" do
+      before do
+        sign_in follower
+      end
+
+      it "リクエストが成功すること" do
+        delete user_follow_path(followed.id, follow.id), xhr: true
+        expect(response.status).to eq 200
+      end
+      it "フォロー関係が削除されること" do
+        expect do
+          delete user_follow_path(followed.id, follow.id), xhr: true
+        end.to change(follower.following_relationship, :count).by(-1)
+      end
+    end
+  end
+end
